786dea9e4fd9fe00808bb2bf7795af630462c274
Felipe <chobas@GLaDOS.local>
2016-12-06 21:53:05 -0200

    Deck1 file updated
    deck1.rb is now generating cards with symbols. Text needs fixing.

--- princesaguerreira/deck1.rb
+++ princesaguerreira/deck1.rb
@@ -5,15 +5,10 @@ Squib::Deck.new(cards: 20, layout: 'hand.yml') do
   rect layout: 'cut'
 #  rect layout: 'safe'
   deck = csv file: 'cartas1.csv'
-  svg file: deck['arte']
-
-  %w('carta').each do |key|
-    svg file: "#{key.downcase}.svg"
-    text str: deck[key]
-  end
+  svg file: deck['arte'], x: 150, y: 250
+#  text str: deck['carta']
+  text str: deck['atributo'], x: 150, y: 350
 
   save_png prefix: 'cartas1_'
   save_pdf file: 'cartas1_frente.pdf', trim: 37.5
-  showcase file: 'showcase_cartas1.png', fill_color: '#000000'
-  hand file: 'hand_cartas1.png', trim: 37.5, trim_radius: 25, fill_color: '#00000'
 end
