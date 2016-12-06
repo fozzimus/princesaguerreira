fc4de3bd65701b910145b99a45f15f685c160d3f
Felipe <fozzimus@gmail.com>
2016-12-06 17:31:17 -0200

    Added icons and adjustments to deck files
    Pretty much explanatory: the icons have been added.

--- princesaguerreira/deck2.rb
+++ princesaguerreira/deck2.rb
@@ -1,20 +1,22 @@
 require 'squib'
 
-Squib::Deck.new(cards: 20) do
-  background color: '#919191'
+Squib::Deck.new(cards: 32, layout 'hand.yml') do
+  background color: '#ffffff'
   rect layout: 'cut'
 #  rect layout: 'safe'
-  deck = csv file: 'cartas1.csv'
+  deck = csv file: 'cartas2.csv'
   svg file: deck['arte']
 
-  %w(carta).each do |key|
+  %w('carta').each do |key|
     text str: deck[key]
+    font: Arial, Sans 50
+    align: center
     svg file: "#{key.downcase}.svg"
     text str: deck[key]
   end
 
-  save_png prefix: 'cartas1_'
-  save_pdf file: 'cartas1_frente.pdf', trim: 37.5
-  showcase file: 'showcase_cartas1.png', fill_color: '#0000'
-  hand file: 'hand_cartas1.png', trim: 37.5, trim_radius: 25, fill_color: '#0000'
+  save_png prefix: 'cartas2_'
+  save_pdf file: 'cartas2_frente-tras.pdf', trim: 37.5
+  showcase file: 'showcase_cartas2.png', fill_color: '#000000'
+  hand file: 'hand_cartas2.png', trim: 37.5, trim_radius: 25, fill_color: '#000000'
 end
